a1cedf4ee81d14041ac3b08a7763d826439d3968
Peter Trifanov <peter.trifanov@mail.ru>
2021-03-10 10:43:17 +0300

    Removed redundant dependencies from build.gradle.kts
    ### What's done:
    * kotlin("jvm") applies java plugin
    * some ktor dependencies are already included as transitive

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,11 @@ repositories {
 }
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.38.0"
-    java
     kotlin("jvm") version "1.4.21"
     kotlin("plugin.serialization") version "1.4.21"
     application
     id("org.cqfn.diktat.diktat-gradle-plugin") version "0.4.2"
+    id("com.github.ben-manes.versions") version "0.38.0"
 }
 
 tasks.withType<KotlinCompile> {
@@ -27,13 +26,8 @@ tasks.withType<KotlinCompile> {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
-    implementation("io.ktor:ktor-client-auth-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-client-serialization-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
     implementation("io.ktor:ktor-client-auth-basic:$ktorVersion")
     implementation("io.github.microutils:kotlin-logging:1.12.0")
 
